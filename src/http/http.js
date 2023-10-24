91fb522f4c71868981304a3e0c47d0bfe901d18a
stevendeyou <stevendeyou@outlook.com>
2023-10-24 11:15:07 +0800

    Added CORS support

--- src/http/http.js
+++ src/http/http.js
@@ -2,6 +2,7 @@ import express from "express";
 import { appRoutes } from './httpRoutes.js';
 import { TestUtil } from "chaintalk-utils";
 import { ParamUtils } from "../utils/ParamUtils.js";
+import cors from "cors";
 
 const http = express();
 const port = ParamUtils.getHttpPort();
@@ -18,6 +19,14 @@ export function runApp()
 	http.use( express.static( 'public' ) );	//	static files
 	http.use( express.json() );
 	http.use( express.urlencoded( { extended : true } ) );
+
+	http.use( cors( {
+		origin : '*',					//	允许所有来源
+		methods : 'GET,HEAD,PUT,PATCH,POST,DELETE',	//	允许的 HTTP 方法
+		preflightContinue : false,
+		credentials : true,				//	允许发送 cookie
+		optionsSuccessStatus : 204			//	对于预检请求，返回 204
+	} ) );
 
 	//	...
 	appRoutes( http );
